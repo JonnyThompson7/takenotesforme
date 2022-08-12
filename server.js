5e2b726307404c722560a4dbaa3a4e488256ca3f
Jonathan Thompson <jathompson399@gmail.com>
2022-08-11 21:50:54 -0500

    create routes

--- server.js
+++ server.js
@@ -25,11 +25,23 @@ app.get('/api/notes', (req, res) => {
   res.json(results);
 })
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
 app.post('/api/notes', (req, res) => {
   req.body.id = notes.length.toString();
   const newNote = createNewNote(req.body, notes);
   res.json(newNotes);
 })
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on PORT ${PORT}!`);
